43f71d9902a24546083217164d8639ee3b38886f
Te <worksingletonmcdaniel@gmail.com>
2022-11-13 14:50:30 -0800

    complete product todos

--- models/Product.js
+++ models/Product.js
@@ -9,32 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-    id:{
-      type: DataTypes.INTERGER,
-      allowNull:false,
+    // HW13 TODO define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
-      autoIncrement:true,
+      autoIncrement: true,
     },
-    product_name:{
-      type:DataTypes.STRING,
-      allowNull: false, 
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
-      type:DataTypes.DECIMAL(10,2),
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
-      validate:{
+      validate: {
         isDecimal: true
       }
     },
-    stock:{
-      type:DataTypes.INTERGER,
+    stock: {
+      type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      validate{
+      validate: {
         isNumeric: true
       }
-    }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      }
+    },
   },
   {
     sequelize,
