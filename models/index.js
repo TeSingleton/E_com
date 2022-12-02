0789ba7c1a2db5629b7b8a8328501d9a24e8ff39
Te <worksingletonmcdaniel@gmail.com>
2022-12-01 16:33:42 -0800

    Create CAT findOne by ID

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // import models
 const Product = require("./Product");
-const Category = require("./Catergory");
+const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,31 +1,41 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
-// todo add routes 
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+// todo add routes
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Category.findAll({
     // include its associated Products
-    include: [Product]
+
+    include: [Product],
   })
-  .then(categories => res.json(categories))
+    .then((categories) => res.json(categories))
+    .catch((err) => res.status(500).json(err));
+});
+
+router.get("/:id", (req, res) => {
+  // find one category by its `id` value
+  Catergory.findOne({ where: { id: req.params.id }, include: [Product] })  .then(category => {
+    // if category doesn't exist, return 404 error
+    if(!category) {
+      res.status(404).json({ message: 'Nothing found with that ID.' });
+      return;
+    }
+    res.json(category);
+  })
   .catch(err => res.status(500).json(err));
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
-
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
+  
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
