8eb9efb63efff3f3a10aea7eb39b03ec7a353b7a
Te <worksingletonmcdaniel@gmail.com>
2022-12-02 17:24:07 -0800

    added Tag routes with source citation

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,6 +3,12 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
+/*  
+    Adapted from...  
+    Title: ecommerce-backend
+    Author: kaiafay
+  Availability: https://github.com/kaiafay/ecommerce-backend*/
+
 router.get("/", (req, res) => {
   // find all tags
   Tag.findAll({
@@ -16,22 +22,48 @@ router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   Tag.findOne({
-    where:{id:req.params.id},
-    include:[{model:Product, through:ProductTag}]
-  })
+    where: { id: req.params.id },
+    include: [{ model: Product, through: ProductTag }],
+  }).then((tag) => {
+    if (!tag) {
+      res.status(400).json({ message: "No tag found with ID" });
+      return;
+    }
+  });
 });
 
 router.post("/", (req, res) => {
   // create a new tag
-  Tag.create
+  Tag.create(req.body)
+    .then((newTag) => res.json(newTag))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: { id: req.params.id },
+  }).then((updatedTag) => {
+    if (!updatedTag) {
+      res.status(400).json({ message: "No tag found with ID" });
+      return;
+    }
+  });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: " No tag found with ID " });
+      }
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
